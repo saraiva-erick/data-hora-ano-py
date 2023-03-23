41b671a6cbe5e60290f618812e781a21176144b9
saraiva-erick <127536402+saraiva-erick@users.noreply.github.com>
2023-03-23 14:14:35 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def main():
     # Obter a data e hora atual
     now = datetime.datetime.now()
 
@@ -12,12 +12,10 @@ def index():
     end_of_year = datetime.datetime(now.year, 12, 31)
     days_left = (end_of_year - now).days
 
-    # Renderizar o template HTML com os dados
-    return render_template('index.html',
-                           weekday=now.strftime("%A"),
-                           date=now.strftime("%d/%m/%Y"),
-                           days_passed=now.timetuple().tm_yday,
-                           days_left=days_left)
+    # Retornar os dados
+    return ('<h1>Bem-vindo(a) ao programa de data e hora em Flask/Python!</h1>',
+            '<p> Hoje é: ' + now.strftime("%A") +' - ' + now.strftime("%d/%m/%Y") + '</p>',
+            '<p> Já passaram ' + str(now.timetuple().tm_yday) + ' dias este ano e restam ' + str(days_left) + ' dias até o final do ano</p>')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
