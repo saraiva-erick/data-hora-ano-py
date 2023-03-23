30bead55c1d8340919363ed1eff0df3e0904a011
saraiva-erick <127536402+saraiva-erick@users.noreply.github.com>
2023-03-23 14:55:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ def main():
 
     # Retornar os dados
     return ('<h1>Bem-vindo(a) ao programa de data e hora em Flask/Python!</h1>',
-            '<p> Hoje é: ' + now.strftime("%A") +' - ' + now.strftime("%d/%m/%Y") + '</p>',
-            '<p> Já passaram ' + str(now.timetuple().tm_yday) + ' dias este ano e restam ' + str(days_left) + ' dias até o final do ano</p>')
+            '<p> Hoje é: ' + now.strftime("%A") '</p>')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
